5fda6584407f44899d1189149b09e52cee90eb20
kimengu-david <davidmutune200@gmail.com>
2021-08-05 20:27:19 +0000

    Handle signal using keyboardInterrupt exception

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,8 +3,6 @@
 
 import re
 import sys
-from signal import signal, SIGINT
-import traceback
 counter = 1
 file_size = 0
 statusC_counter = {200: 0, 301: 0, 400: 0,
@@ -13,28 +11,24 @@ statusC_counter = {200: 0, 301: 0, 400: 0,
 
 def printCodes(dict, file_s):
     """Prints the status code and the number of times they appear"""
-    print("File size: {}".format(file_s), flush=True)
+    print("File size: {}".format(file_s))
     for key in sorted(dict.keys()):
         if statusC_counter[key] != 0:
-            print("{}: {}".format(key, dict[key]), flush=True)
+            print("{}: {}".format(key, dict[key]))
 
 
-def handler(signal_received, frame):
-    """Handles the ctrl-c signal."""
-    print(end="", flush=True)
+try:
+    for line in sys.stdin:
+        split_string = re.split('- |"|"| " " ', str(line))
+        statusC_and_file_s = split_string[-1]
+        statusC = int(statusC_and_file_s.split()[0])
+        f_size = int(statusC_and_file_s.split()[1])
+        if statusC in statusC_counter:
+            statusC_counter[statusC] = statusC_counter[statusC] + 1
+            file_size = file_size + f_size
+        if counter % 10 == 0:
+            printCodes(statusC_counter, file_size)
+        counter = counter + 1
+except KeyboardInterrupt:
     printCodes(statusC_counter, file_size)
-    exit(0)
-
-
-for line in sys.stdin:
-    split_string = re.split('- |"|"| " " ', str(line))
-    statusC_and_file_s = split_string[-1]
-    statusC = int(statusC_and_file_s.split()[0])
-    f_size = int(statusC_and_file_s.split()[1])
-    if statusC in statusC_counter:
-        statusC_counter[statusC] = statusC_counter[statusC] + 1
-        file_size = file_size + f_size
-    if counter % 10 == 0:
-        printCodes(statusC_counter, file_size)
-    signal(SIGINT, handler)
-    counter = counter + 1
+    raise
