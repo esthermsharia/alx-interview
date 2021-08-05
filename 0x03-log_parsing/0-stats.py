47ce1807edee00be305aae285f63c4c785b6a126
kimengu-david <davidmutune200@gmail.com>
2021-08-05 23:10:38 +0000

    Run file as the main program

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,20 +16,20 @@ def printCodes(dict, file_s):
         if statusC_counter[key] != 0:
             print("{}: {}".format(key, dict[key]), flush=True)
 
-
-try:
-    for line in sys.stdin:
-        split_string = re.split('- |"|"| " " ', str(line))
-        statusC_and_file_s = split_string[-1]
-        statusC = int(statusC_and_file_s.split()[0])
-        f_size = int(statusC_and_file_s.split()[1])
-        # print("Status Code {} size {}".format(statusC, f_size))
-        if statusC in statusC_counter:
-            statusC_counter[statusC] = statusC_counter[statusC] + 1
-            file_size = file_size + f_size
-        if counter != 0 and counter % 10 == 0:
-            printCodes(statusC_counter, file_size)
-        counter = counter + 1
-except KeyboardInterrupt:
-    printCodes(statusC_counter, file_size)
-    raise
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            statusC = int(statusC_and_file_s.split()[0])
+            f_size = int(statusC_and_file_s.split()[1])
+            # print("Status Code {} size {}".format(statusC, f_size))
+            if statusC in statusC_counter:
+                statusC_counter[statusC] = statusC_counter[statusC] + 1
+                file_size = file_size + f_size
+            if counter != 0 and counter % 10 == 0:
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
+    except KeyboardInterrupt:
+        printCodes(statusC_counter, file_size)
+        raise
